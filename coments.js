27d2d9ab822976eb2a221f61892fb25d2aed7bfe
maucoder <maugfalcon1@gmail.com>
2021-08-05 16:26:31 -0400

    Creating arrow functions and optimizating our code.

---
+++ coments.js
@@ -0,0 +1,82 @@
+// let square_sides = prompt("write square sides in cm");
+// console.log("square's misure are " + square_sides + "cm");
+
+// square's code
+// console.group("Squares")
+
+
+// function perimeter(option){
+//     if(option == 1){
+//         return square_sides * square_sides
+//     }
+//     else if (option == 2){
+//         return triangle_side0 + triangle_side_left + triangle_base
+//     }
+//     else{
+//         return (circle_radium * circle_radium) * PI
+//     }
+// }
+
+
+// const square_perimeter = square_sides * 4;
+// console.log("square perimeter is " + square_perimeter + "cm");
+
+
+// function area(side_base, side_left, side_right){
+//     sides = side_base * side_left
+
+//     if (operation == 1){
+//         figure_area = sides;
+//     }
+//     else if(operation == 2){
+//         figure_area = sides / 2
+//     }
+//     else{
+//         figure_area = sides * PI
+//     }
+// }
+
+// const square_area = square_sides * square_sides;
+// console.log("scuare area is " + square_area + "cmˆ2");
+
+// triangle's code
+
+// console.log
+// (
+//     "triangle side's misure " 
+//     + triangle_side0 + "cm, " 
+//     + triangle_side_left + "cm, " 
+//     + triangle_base + "cm"
+// )
+
+
+// console.log("PI is " + PI)
+
+// perimeter or circunference
+// const circle_circunference = circle_diameter * PI;
+// console.log("circle's circunference is " + circle_circunference + "cm")
+
+// Diameter
+// const circle_diameter = circle_radium * 2;
+// console.log("circle's diameter = " + circle_diameter + "cm")
+
+// PI
+
+// const perimeter_triangles = triangle_side0 + triangle_side_left + triangle_base;
+// console.log("triangle perimeter is " + perimeter_triangles + "cm");
+
+// const area_triangles = (triangle_base * triangle_height) / 2;
+// console.log("area's triangle is " + area_triangles + "cmˆ2")
+
+// console.groupEnd();
+
+// // circle's code
+// console.group("circles");
+
+// // radium
+
+// Area 
+// const circle_area = (circle_radium * circle_radium) * PI
+// console.log("circle's area is " + circle_area + "cmˆ2")
+
+// console.groupEnd();
---
+++ figures.js
@@ -0,0 +1,19 @@
+const square_perimeter = (sides) => sides * 4;
+const triangle_perimeter = (side_base, side_left, side_right) => side_base + side_left + side_right;
+const circle_perimeter = (circle_radium) => (circle_radium ** 2) * PI
+
+const squares_area = (side) => side**2
+const triangle_area = (side_base, triangle_height) => (side_base * triangle_height) / 2 
+const circle_area = (circle_radium) =>(circle_radium ** 2) * PI
+
+let squeare_side = 2
+
+let triangle_side0 = 8;
+let triangle_side_left = 6;
+let triangle_base = 6;
+
+let triangle_height = 5.5;
+
+let circle_radium = 4;
+
+const PI = Math.PI;
