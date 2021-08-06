67f6cecd09586b124c5bf8adc65e9a95a3a5f5bf
maucoder <maugfalcon1@gmail.com>
2021-08-06 16:32:46 -0400

    Creating JS folder and functions with our imputs. Vinculing Html with JavaScript functions. comments and figures with .js extentions was move to JS Folder

--- JS/figures.js
+++ JS/figures.js
@@ -1,8 +1,10 @@
+// Create functions of calculator
+
 const square_perimeter = (sides) => sides * 4;
 const triangle_perimeter = (side_base, side_left, side_right) => side_base + side_left + side_right;
 const circle_perimeter = (circle_radium) => (circle_radium ** 2) * PI
 
-const squares_area = (side) => side**2
+const square_area = (side) => side**2
 const triangle_area = (side_base, triangle_height) => (side_base * triangle_height) / 2 
 const circle_area = (circle_radium) =>(circle_radium ** 2) * PI
 
@@ -11,9 +13,26 @@ let squeare_side = 2
 let triangle_side0 = 8;
 let triangle_side_left = 6;
 let triangle_base = 6;
-
 let triangle_height = 5.5;
 
 let circle_radium = 4;
 
 const PI = Math.PI;
+
+// interactions with HTML
+
+function perimeter_square_calculator(){
+    const input = document.getElementById("input_square_calculator");
+    const value = input.value;
+
+    alert(square_perimeter(value))
+}
+
+function area_square_calculator (){
+    const input = document.getElementById("input_square_calculator");
+    const value = input.value;
+
+    alert(square_area(value))
+}
+
+
