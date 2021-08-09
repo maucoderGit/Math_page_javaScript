fbf4bb6182952fc197748043a35584596630d297
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-09 16:52:13 -0400

    creating function of average.

---
+++ JS/average.js
@@ -0,0 +1,9 @@
+function calcule_aritmetic_average(list){
+    let new_list = 0;
+    for (i of list){
+    new_list += i
+    }
+
+    const average_list = new_list / prices.length
+    return average_list
+}
