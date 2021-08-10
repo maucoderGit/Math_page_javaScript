fa3593797e4dd82798cfd5e1a76d44280bcf4f6c
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-09 23:24:29 -0400

    setting formules.

--- JS/average.js
+++ JS/average.js
@@ -1,9 +1,15 @@
 function calcule_aritmetic_average(list){
-    let new_list = 0;
-    for (i of list){
-    new_list += i
-    }
+    // let new_list = 0;
+    // for (i of list){
+    // new_list += i
+    // }
+    
+    const sum_list = list.reduce(
+        (value_acumulated = 0, new_lement) => {
+            return value_acumulated + new_lement    
+        }
+    ) 
 
-    const average_list = new_list / list.length
+    const average_list = sum_list / list.length
     return average_list
 }
