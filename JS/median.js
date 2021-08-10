821e068fd8d2d204f538b7bf9bc7586819699f16
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-10 15:44:57 -0400

    Exporting and importing funcion of average.js

--- JS/median.js
+++ JS/median.js
@@ -1,4 +1,6 @@
-const LIST = [100, 200, 300, 500, 40000000];
+import { calcule_aritmetic_average } from './average';
+
+const LIST = [0 ,100, 200, 300, 500, 40000000];
 
 const midle_list = parseInt(LIST.length / 2);
 
@@ -14,7 +16,11 @@ function is_list_even(number){
 let median;
 
 if (is_list_even(LIST.length)){
-    
+    const element1 = LIST[midle_list];
+    const element2 = LIST[midle_list + 1];
+    console.log(`elements are: ${element1} and ${element2}`)
+
+    calcule_aritmetic_average([element1, element2])
 } 
 else 
 {  
