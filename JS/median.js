9147dc46a926bab2802eeb93ce92cdf720fe77b0
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-10 15:09:11 -0400

    Creation of formules to calcule median in a list.

---
+++ JS/median.js
@@ -0,0 +1,22 @@
+const LIST = [100, 200, 500, 40000000];
+
+// const midleList = list.length / 2;
+
+function is_list_even(number){
+    if (number % 2 === 0){
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+let median;
+
+if (is_list_even(list.length)){
+    
+} 
+else 
+{
+    
+}
