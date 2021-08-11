0017fd7e74b1bc7c3e15e4c3c7e98b069b03c7e9
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-10 21:20:48 -0400

    Formules of armonic median added.

--- JS/armonic_median.js
+++ JS/armonic_median.js
@@ -1,10 +1,27 @@
 const LIST = [9,4,6,8,8,12];
 
+// function fraction_calculetor(list){
+//     let value = 0;
+
+//     for(i of list){
+//         value += 1/i;
+//     }
+//     console.log(value)
+// }
+
 function armonic_median_calculator(list){
     list.sort((previus_element, new_element) => previus_element - new_element)
     const list_elements = list.length;
 
-    console.log(list)
+    let sum_all_values = list.reduce((element_a,element_b) => {
+            element_a + 1 / element_b
+        })
+
+    
 }
 
-armonic_median_calculator(LIST)
+//     const armonic_median = list_elements / value
+// }
+
+// fraction_calculetor(LIST)
+// armonic_median_calculator(LIST)
