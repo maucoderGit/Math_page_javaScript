2df8348fea823718a731ee9093c5a0d0927d1121
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-11 00:18:06 -0400

    Completing 3rd challenge in JS practice course

--- JS/armonic_median.js
+++ JS/armonic_median.js
@@ -1,5 +1,11 @@
 const LIST = [9,4,6,8,8,12];
 
+function sum_fractions(numerator_a,denominator_a = 1,numerator_b = 1,denominator_b = 1){
+    let result = ((numerator_a * denominator_b) + (denominator_a * numerator_b) 
+                / (denominator_a * denominator_b))
+    console.log(result)
+}
+
 // function fraction_calculetor(list){
 //     let value = 0;
 
@@ -10,19 +16,14 @@ const LIST = [9,4,6,8,8,12];
 // }
 
 function armonic_median_calculator(list){
-    list.sort((previus_element, new_element) => previus_element - new_element)
+    list = list.sort((previus_element, new_element) => previus_element - new_element)
     const list_elements = list.length;
-
-    let sum_all_values = list.reduce((element_a,element_b) => {
-            element_a + 1 / element_b
-        })
+    const reducer = (element_a,element_b) => sum_fractions(1, element_a, 1, element_b)
+    console.log(list_elements)
+  
+    const sum_all_values = list.reduce(reducer)
+    console.log(sum_all_values)
 
     const armonic_median = list_elements / sum_all_values
-    console.log(armonic_median)
+    return armonic_median
 }
-
-//     const armonic_median = list_elements / value
-// }
-
-// fraction_calculetor(LIST)
-armonic_median_calculator(LIST)
