9eb8b5c467982186f9df7b97c8b684c13f4dfa33
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-12 14:26:26 -0400

    method .splice learned and use in analytics.js

--- JS/analytics.js
+++ JS/analytics.js
@@ -1,3 +1,9 @@
+//Helpers
+
+function is_even(number){
+    return (number % 2 === 0);
+}
+
 function calcule_aritmetic_average(list){
     const sum_list = list.reduce(
         (value_acumulated = 0, new_lement) => {
@@ -9,21 +15,8 @@ function calcule_aritmetic_average(list){
     return average_list
 }
 
-const salaries = COLOMBIA.map(
-    function(citizen){
-        return citizen.salary;
-    }
-);
+// Median Calculator
 
-const salaries_sorted = salaries.sort(
-    function (salary_a, salary_b) {
-        return salary_a - salary_b;
-    }
-);
-
-function is_even(number){
-    return (number % 2 === 0);
-}
 
 function median_salaries(list){
     const midle = parseInt(list.length / 2)
@@ -40,4 +33,32 @@ function median_salaries(list){
     }
 }
 
+
+//Median general
+
+const salaries = COLOMBIA.map(
+    function(citizen){
+        return citizen.salary;
+    }
+);
+
+const salaries_sorted = salaries.sort(
+    function (salary_a, salary_b) {
+        return salary_a - salary_b;
+    }
+);
+
 const median_general = median_salaries(salaries_sorted)
+
+// top 10% median.
+const splice_start = (salaries_sorted.length * 90) / 100;
+const splice_count = salaries_sorted.length - splice_start;
+
+const salaries_top10 = salaries_sorted.splice(
+    splice_start, splice_count
+)
+
+console.log({
+    median_general,
+    medianTop10,
+})
