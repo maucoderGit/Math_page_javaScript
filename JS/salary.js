b36edd647528356ebe1621b42e768376b27d5154
maucoderGit <dreg5000.bbb@gmail.com>
2021-08-12 13:02:08 -0400

    rename of salary.js to analytics.js

---
+++ JS/salary.js
@@ -0,0 +1,83 @@
+const COLOMBIA = [];
+
+COLOMBIA.push({
+    name: "Camila",
+    salary: 500,
+  });
+  COLOMBIA.push({
+    name: "Nath",
+    salary: 1500,
+  });
+  COLOMBIA.push({
+    name: "Luisa",
+    salary: 1800,
+  });
+  COLOMBIA.push({
+    name: "Laura",
+    salary: 1000,
+  });
+  COLOMBIA.push({
+    name: "Daniela",
+    salary: 2200,
+  });
+  COLOMBIA.push({
+    name: "Esperancita",
+    salary: 200,
+  });
+  COLOMBIA.push({
+    name: "Carla",
+    salary: 500,
+  });
+  COLOMBIA.push({
+    name: "Antonieta",
+    salary: 1500,
+  });
+  COLOMBIA.push({
+    name: "Alicia",
+    salary: 1300,
+  });
+  COLOMBIA.push({
+    name: "Ana",
+    salary: 2400,
+  });
+  COLOMBIA.push({
+    name: "Julia",
+    salary: 3400,
+  });
+  COLOMBIA.push({
+    name: "Rosa",
+    salary: 400,
+  });
+  COLOMBIA.push({
+    name: "Angélica",
+    salary: 400,
+  });
+  COLOMBIA.push({
+    name: "Tatiana",
+    salary: 400,
+  });
+  COLOMBIA.push({
+    name: "Lorena",
+    salary: 600,
+  });
+  COLOMBIA.push({
+    name: "Carolina",
+    salary: 1600,
+  });
+  COLOMBIA.push({
+    name: "Fernanda",
+    salary: 2600,
+  });
+  COLOMBIA.push({
+    name: "Nora",
+    salary: 1000,
+  });
+  COLOMBIA.push({
+    name: "Gisselle",
+    salary: 2000,
+  });
+  COLOMBIA.push({
+    name: "Bill Gates",
+    salary: 100000000,
+  });
+  
